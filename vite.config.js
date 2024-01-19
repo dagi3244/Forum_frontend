5a954061f865aa31318a0eb1714b568fd974b066
dagi3244 <tttfffqr@gmail.com>
2024-01-19 08:06:14 -0800

    m

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/dist",
   build: {
     outDir: "dist", // Specify the output directory
   },
